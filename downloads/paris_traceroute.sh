6f1b1a7af627d97d2836a2fa6a4ab0c43a28bd53
rickmcgeer <rick@mcgeer.com>
2019-05-25 18:09:21 -0700

    Added Paris Traceroute tutorial

--- downloads/paris_traceroute.sh
+++ downloads/paris_traceroute.sh
@@ -2,4 +2,4 @@
 # kubectl exec multilevel-mda-lite-paristraceroute-6fxbd -- bash -c "python3 multilevel-mda-lite/MDALite.py 142.104.197.120"
 # ./paris-traceroute.sh multilevel-mda-lite-paristraceroute-6fxbd 142.104.197.120
 # -paristraceroute-6fxbd -- bash -c "python3 multilevel-mda-lite/MDALite.py 142.104.197.120"
-kubectl exec $1 -- bash -c "python3 multilevel-mda-lite/MDALite.py $2"
+kubectl exec multilevel-mda-lite-paristraceroute-$1 -- bash -c "python3 multilevel-mda-lite/MDALite.py $2"
